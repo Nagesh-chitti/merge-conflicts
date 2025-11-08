08f30118f889c388b9818bbd0428eebdfb44d2f7
Nagesh-chitti <nageshchitti2611@gmail.com>
2025-11-08 13:57:01 +0530

    Update app.java

--- app.java
+++ app.java
@@ -3,3 +3,13 @@ public class Main {
        System.out.println("Hello World");
    }
 }
+
+//dev-02 push some local 
+public class Main{
+   public static void main(String[] args) {
+      int a=2;
+      int b=3;
+      int c=a+b;
+      System.out.println("sum of two number " + " c);
+   }
+}
